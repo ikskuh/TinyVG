13a9cfc2b6136a21ada3835c943a522af1d9e863
Felix (xq) Queißner <git@mq32.de>
2021-04-17 22:24:40 +0200

    Adds auto-generated examples info

---
+++ examples/README.md
@@ -0,0 +1,37 @@
+# Example Files
+
+## `app_menu.tvg`
+
+![](app_menu.png)
+
+**TVG Size**: 72 (100%)
+**PNG Size**: 86 (119%)
+
+## `feature-showcase.tvg`
+
+![](feature-showcase.png)
+
+**TVG Size**: 1247 (100%)
+**PNG Size**: 26567 (2130%)
+
+## `shield.tvg`
+
+![](shield.png)
+
+**TVG Size**: 207 (100%)
+**PNG Size**: 169 (81%)
+
+## `workspace_add.tvg`
+
+![](workspace_add.png)
+
+**TVG Size**: 85 (100%)
+**PNG Size**: 126 (148%)
+
+## `workspace.tvg`
+
+![](workspace.png)
+
+**TVG Size**: 56 (100%)
+**PNG Size**: 120 (214%)
+
--- info.sh
+++ info.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+set -e
+
+# First, generate all example files
 zig build generate
 
+(
+  echo '# Example Files'
+  echo
+) > examples/README.md
+
 for file in examples/*.tvg ; do
+  PNG_FILE="${file%.*}.png"
   SIZE_TVG="$(wc -c "${file}" | awk '{ print $1 }')"
-  SIZE_PNG="$(wc -c "${file%.*}.png" | awk '{ print $1 }')"
-  echo "${file} has size ${SIZE_TVG}, which are $(( (100 * ${SIZE_TVG} ) / ${SIZE_PNG} ))% of ${SIZE_PNG}"
+  SIZE_PNG="$(wc -c "${PNG_FILE}" | awk '{ print $1 }')"
+  (
+    echo "## \`$(basename "${file}")\`"
+    echo ""
+    echo "![]($(basename "${PNG_FILE}"))"
+    echo ""
+    echo "**TVG Size**: ${SIZE_TVG} (100%)"
+    echo "**PNG Size**: ${SIZE_PNG} ($(( (100 * ${SIZE_PNG} ) / ${SIZE_TVG} ))%)"
+    echo ""
+  ) >> examples/README.md
 done
+
+cat examples/README.md
